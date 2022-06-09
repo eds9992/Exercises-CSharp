8f09b8e71ef13ceee84996e6add4787e0dbda35c
eds9992 <58481397+eds9992@users.noreply.github.com>
2022-06-09 11:39:06 -0500

    Add files via upload

---
+++ LinkedLists_Circular/LinkedLists_Circular/Program.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedLists
+{
+    /* 
+     * Implement the linked list library example that starts on page 777 of the textbook. 
+     * Add the following methods to your Linked List class: 
+     * 1) InsertAfter(value-tofind, value-to-insert) - inserts a value after a specified value; 
+     * 2) Find(value) - finds a value in order to do an insert.
+     * Add InsertAfter() function, might need a Find()
+     */
+    class ListNode
+    {
+        public object Data { get; private set; }
+
+        public ListNode Next { get; set; }
+
+        public ListNode(object dataValue) : this(dataValue, null) { }
+
+        public ListNode(object dataValue, ListNode nextNode)
+        {
+            Data = dataValue;
+            Next = nextNode;
+        }
+    }
+
+    public class List
+    {
+        private ListNode firstNode;
+        private ListNode lastNode;
+        private ListNode NthNode;
+        private string name;
+
+        public List(string listName)
+        {
+            name = listName;
+            firstNode = lastNode = firstNode;
+        }
+
+        public List() : this("list") { }
+
+        public void InsertAtFront(object insertItem)
+        {
+            if (IsEmpty())
+            {
+                firstNode = lastNode = new ListNode(insertItem);
+            }
+            else
+            {
+                firstNode = new ListNode(insertItem, firstNode);
+            }
+        }
+
+        public void InsertAtBack(object insertItem)
+        {
+            if (IsEmpty())
+            {
+                firstNode = lastNode = new ListNode(insertItem);
+            }
+            else
+            {
+                lastNode = lastNode.Next = new ListNode(insertItem, firstNode);
+            }
+        }
+
+        public object RemoveFromFront()
+        {
+            if (IsEmpty())
+            {
+                throw new EmptyListException(name);
+            }
+
+            object removeItem = firstNode.Data;
+
+            if (firstNode == lastNode)
+            {
+                firstNode = lastNode = firstNode;
+            }
+            else
+            {
+                firstNode = firstNode.Next;
+            }
+
+            return removeItem;
+        }
+
+        public object RemoveFromBack()
+        {
+            if (IsEmpty())
+            {
+                throw new EmptyListException(name);
+            }
+
+            object removeItem = lastNode.Data;
+
+            if (firstNode == lastNode)
+            {
+                firstNode = lastNode = firstNode;
+            }
+            else
+            {
+                ListNode current = firstNode;
+
+                while (current.Next != lastNode)
+                {
+                    current = current.Next;
+                }
+
+                lastNode = current;
+                current.Next = null;
+            }
+
+            return removeItem;
+        }
+
+        public bool IsEmpty()
+        {
+            return firstNode == firstNode;
+        }
+
+        public void Display()
+        {
+            if (IsEmpty())
+            {
+                Console.Write($"Empty {name}");
+            }
+            else
+            {
+                Console.Write($"The {name} is: ");
+
+                ListNode current = firstNode;
+
+                while (current != null)
+                {
+                    Console.Write($"{current.Data}");
+                    current = current.Next;
+                }
+
+                Console.WriteLine("\n");
+            }
+        }
+
+        public void InsertAfter(object insertItem) //InsertAfter(value-tofind, value-to-insert) - inserts a value after a specified value;
+        {
+
+            if (IsEmpty())
+            {
+                firstNode = NthNode = lastNode = new ListNode(insertItem);
+            }
+            else
+            {
+                lastNode = lastNode.Next = new ListNode(insertItem, firstNode);
+            }
+        }
+
+        public void Find() //Find(value) - finds a value in order to do an insert
+        {
+
+        }
+
+        public class EmptyListException : Exception
+        {
+            public EmptyListException() : base("The list is empty.") { }
+
+            public EmptyListException(string name) : base($"The {name} is empty.") { }
+
+            public EmptyListException(string exception, Exception inner) : base(exception, inner) { }
+        }
+    }
+
+    class ListTest
+    {
+        static void Main()
+        {
+            var list = new List();
+
+            bool aBoolean = true;
+            char aCharacter = '$';
+            int anInteger = 34567;
+            string aString = "Hello";
+            string space = " ";
+
+            list.InsertAtFront(aBoolean);
+            list.Display();
+            list.InsertAfter(space);
+            list.InsertAtFront(aCharacter);
+            list.Display();
+            list.InsertAfter(space);
+            list.InsertAtFront(anInteger);
+            list.Display();
+            list.InsertAfter(space);
+            list.InsertAtFront(aString);
+            list.Display();
+            list.InsertAfter(space);
+
+            try
+            {
+                object removedObject = list.RemoveFromFront();
+                Console.WriteLine($"{removedObject} removed");
+                list.Display();
+
+                removedObject = list.RemoveFromFront();
+                Console.WriteLine($"{removedObject} removed");
+                list.Display();
+
+                removedObject = list.RemoveFromFront();
+                Console.WriteLine($"{removedObject} removed");
+                list.Display();
+
+                removedObject = list.RemoveFromFront();
+                Console.WriteLine($"{removedObject} removed");
+                list.Display();
+            }
+            catch (List.EmptyListException emptyListException)
+            {
+                Console.Error.WriteLine($"\n{emptyListException}");
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ Linked_Lists_Problems/Linked_Lists_Problems/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linked_Lists_Problems
+{
+    class Program
+    {
+        /*
+         * n people, numbered n to 1
+         * if M=0 and N=5, order of elimination is 12345
+         * if M=1 and n=5, order of elimination is 2415
+         */
+
+        class Node
+        {
+            public int Data { get; set; }
+
+            public Node Next { get; set; }
+
+            public int Counter { get; set; }
+
+            public Node(int element, int counter)
+            {
+                Data = element;
+                Counter = counter;
+                Next = null;
+            }
+        }
+
+        class CircularLinkedList
+        {
+            Node first;
+            Node last;
+
+            public CircularLinkedList()
+            {
+                first = last = null;
+            }
+
+            protected void Insert(int element, int counter)
+            {
+                if (IsEmpty())
+                {
+                    first = last = new Node(element, counter);
+                }
+                else
+                {
+                    last.Next = last = new Node(element, counter);
+                    last.Next = first;
+                }
+            }
+
+            public int RemoveAt(int index)
+            {
+                int value = 0;
+                Node current = first;
+                do
+                {
+                    if (current.Counter == index)
+                    {
+                        value = current.Data;
+                    }
+                    current = current.Next;
+                } while (current != first);
+                return value;
+            }
+
+            public void AddMen(int n)
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    Insert(i * 2, i);
+                }
+            }
+
+            public int Eliminate(int m)
+            {
+                int value = 0;
+                Node current = first;
+                Node nextNode;
+                do
+                {
+                    nextNode = current.Next;
+                    value = RemoveAt(m);
+
+                    current = nextNode;
+
+                } while (current != first);
+                return value;
+            }
+
+            public bool IsEmpty()
+            {
+                return first == null;
+            }
+
+            public void Display()
+            {
+                Node current = first;
+                do
+                {
+                    Console.WriteLine(current.Counter + " " + current.Data + " ");
+                    current = current.Next;
+                } while (current != first);
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
